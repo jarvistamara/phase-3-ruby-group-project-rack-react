944416e4678556f5081bc9c1c8c15e4d02386976
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-03 11:00:55 -0400

    Classroom routes complete

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -3,14 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-      binding.pry
+binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
     elsif req.path.match(/classrooms/)
-      if req.path.split("/classrooms").length == 0 
+      if req.path.split("/classrooms").length == 0
         return [200, { 'Content-Type' => 'application/json' }, [ Classroom.all.to_json ]]
       else 
-        classroom_id = req.path.split("/classroom/").last 
+        classroom_id = req.path.split("/classrooms/").last 
+        binding.pry
         return [200, { 'Content-Type' => 'application/json' }, [ Classroom.find_by(id: classroom_id).to_json({:include => :students}) ]]
       end
     else
