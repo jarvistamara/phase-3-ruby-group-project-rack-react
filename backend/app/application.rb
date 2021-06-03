7ee5af3fe45bdba4f17e89a0de5953345109c624
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-03 02:05:04 -0400

    trouble with browser, switching to postman

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -10,23 +10,16 @@ class Application
     
       elsif req.path.match(/classrooms/)
       if req.path.split("/classrooms").length == 0 
-        # if there is no id, then this will return 200
+        # if there is no id, then this will return 200 ans show all Classrooms
       
         return [200, { 'Content-Type' => 'application/json' }, [ Classroom.all.to_json ]]
-      else classroom_id = req.path.split("/classroom/").last 
-        #else it has an id so it needs to request only one classroom and not all. 
-      
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+      else 
+        classroom_id = req.path.split("/classroom/").last 
+        # else it has an id so it needs to request only one classroom and not all. 
+        # use find to get a single matching id from the database.
+        Classroom.find_by(id: classroom_id)
+        return [200, { 'Content-Type' => 'application/json' }, [ Classroom.find_by(id: classroom_id).to_json ]]
+      end
     else
       resp.write "Path Not Found"
 
