f2f829b55ebe262ab1ffc3239e3ad05d3b927fd7
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-03 00:53:42 -0400

    working on classroom/id request

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -6,10 +6,27 @@ class Application
       binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-
-    elsif req.path.match(/classrooms/)
-      classrooms = Classroom.all
-      return [200, { 'Content-Type' => 'application/json' }, [ classrooms.to_json ]]
+        # first route, access to all classrooms
+    
+      elsif req.path.match(/classrooms/)
+      if req.path.split("/classrooms").length == 0 
+        # if there is no id, then this will return 200
+      
+        return [200, { 'Content-Type' => 'application/json' }, [ Classroom.all.to_json ]]
+      else classroom_id = req.path.split("/classroom/").last 
+        #else it has an id so it needs to request only one classroom and not all. 
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     else
       resp.write "Path Not Found"
 
