51333219e75af2c31cdc844e99403f21c8655ba1
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-03 02:35:07 -0400

    connection issues not allowing me to check routes

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -6,18 +6,11 @@ class Application
       binding.pry
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-        # first route, access to all classrooms
-    
-      elsif req.path.match(/classrooms/)
+    elsif req.path.match(/classrooms/)
       if req.path.split("/classrooms").length == 0 
-        # if there is no id, then this will return 200 ans show all Classrooms
-      
         return [200, { 'Content-Type' => 'application/json' }, [ Classroom.all.to_json ]]
       else 
         classroom_id = req.path.split("/classroom/").last 
-        # else it has an id so it needs to request only one classroom and not all. 
-        # use find to get a single matching id from the database.
-        Classroom.find_by(id: classroom_id)
         return [200, { 'Content-Type' => 'application/json' }, [ Classroom.find_by(id: classroom_id).to_json ]]
       end
     else
